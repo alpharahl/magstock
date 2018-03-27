52d5b6ac2f538454595d09bc7310d4c335b25092
David Gauntlett <david.m.gauntlett@gmail.com>
2018-03-27 01:48:18 -0400

    going throughout app with navigation buttons now works

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
 var cur_menu = 'landing';
-$(function() {
- $('#about_link').click(function() {
-   showAbout();
- })
-})
+// $(function() {
+//  $('#about_link').click(function() {
+//    showAbout();
+//  })
+// })
 $(function(){
 $('#events_link').click(function(){
   showEvent();
@@ -90,68 +90,110 @@ $(function(){
  })
 })
 
-function showMenu(){
- $('#landing_page').fadeOut(750);
- $('#about').hide();
- $('#directions').hide();
- $('#whattobring').hide();
- $('#signup').hide();
- $('#menu').show();
- $('#header_box').show();
- $('#footer_box').show();
- $('#supportBee').show();
- $('#events').hide();
- $('#music').hide();
- $('#rewards').hide();
- $('#opportunities').hide();
- $('#menuselection').html('');
+function hideAll(){
+  $('#landing_page').fadeOut(750);
+  $('#about').hide();
+  $('#directions').hide();
+  $('#whattobring').hide();
+  $('#signup').hide();
+  $('#menu').hide();
+  $('#events').hide();
+  $('#music').hide();
+  $('#rewards').hide();
+  $('#opportunities').hide();
+  $('#menuselection').html('');
+}
 
- cur_menu = 'main_menu';
- // menuinput.focus();
+function showMenu(){
+  hideAll();
+  $('#menu').show();
+  $('#header_box').show();
+  $('#footer_box').show();
+  $('#menuselection').html('');
+
+  cur_menu = 'main_menu';
+  // menuinput.focus();
 }
 function showAbout(){
- $('#about').show();
- $('#menu').hide();
- $('#aboutselection').html('');
- cur_menu = 'about_menu';
+  hideAll();
+  $('#about').show();
+  $('#menu').hide();
+  $('#aboutselection').html('');
+  cur_menu = 'about_menu';
 }
 function showEvent(){
- $('#about').hide();
- $('#events').show();
- cur_menu = 'return';
+  hideAll();
+  $('#about').hide();
+  $('#events').show();
+  cur_menu = 'return';
 }
 function showMusic(){
- $('#about').hide();
- $('#music').show();
- cur_menu = 'return';
+  hideAll();
+  $('#about').hide();
+  $('#music').show();
+  cur_menu = 'return';
 }
 function showDirections(){
- $('#directions').show();
- $('#menu').hide();
- cur_menu = 'return';
+  hideAll();
+  $('#directions').show();
+  $('#menu').hide();
+  cur_menu = 'return';
 }
 function showWhatToBring(){
- $('#whattobring').show();
- $('#menu').hide();
- cur_menu = 'return';
+  hideAll();
+  $('#whattobring').show();
+  $('#menu').hide();
+  cur_menu = 'return';
 }
 function showSignUp(){
- $('#signup').show();
- $('#menu').hide();
- cur_menu = 'volunteer_menu';
+  hideAll();
+  $('#signup').show();
+  $('#menu').hide();
+  cur_menu = 'volunteer_menu';
 }
 function showOpportunities(){
+  hideAll();
   $('#signup').hide();
   $('#opportunities').show();
   cur_menu = 'return';
 }
 function showRewards(){
+  hideAll();
   $('#signup').hide();
   $('#rewards').show();
   cur_menu = 'return';
 }
 
+
+$(window).hashchange(function(){
+  switch(location.hash){
+    case '#menu':
+      showMenu();
+      break;
+    case '#about':
+      showAbout();
+      break;
+    case '#directions':
+      showDirections();
+      break;
+    case '#what_to_bring':
+      showWhatToBring();
+      break;
+    case '#signup':
+      showSignUp();
+      break;
+    case '#events':
+      showEvent();
+      break;
+    case '#music':
+      showMusic();
+      break;
+  }
+})
+
+
 window.onload = function(){
+ // $(window).hashchange();
  var input = document.getElementById('body_div');
  $(document).keypress(function(e){
    switch(cur_menu){
@@ -164,13 +206,16 @@ window.onload = function(){
        if (e.key == "Enter"){
          switch($('#aboutselection').html()){
            case '1':
-             showEvent();
+             // showEvent();
+             window.location.hash = "#events"
              break;
            case '2':
-             showMusic();
+             // showMusic();
+             window.location.hash = "#music"
              break;
            case '3':
-             showMenu();
+             // showMenu();
+             window.location.hash = "#menu"
              break;
            default:
              $('aboutselection').html('');
@@ -183,20 +228,24 @@ window.onload = function(){
        if (e.key == "Enter"){
           switch($('#menuselection').html()){
             case '1':
-              showAbout();
-              cur_menu = 'about_menu';
+              // showAbout();
+              window.location.hash = "#about"
+              // cur_menu = 'about_menu';
               break;
             case '2':
               window.location.replace("https://magstock8.uber.magfest.org/uber/preregistration/form");
               break;
             case '3':
-              showDirections();
+              // showDirections();
+              window.location.hash = "#directions"
               break;
             case '4':
-              showWhatToBring();
+              // showWhatToBring();
+              window.location.hash = "#what_to_bring"
               break;
             case '5':
-              showSignUp();
+              // showSignUp();
+              window.location.hash = "#signup"
               break;
             default:
               $('menuselection').html('')
