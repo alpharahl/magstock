d3d6b37f13ba3a1db73f50575ca4dbd678305f8e
David Gauntlett <david.m.gauntlett@gmail.com>
2018-03-27 02:10:28 -0400

    add band pages, and more back/forward navigation

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,91 +4,91 @@ var cur_menu = 'landing';
 //    showAbout();
 //  })
 // })
-$(function(){
-$('#events_link').click(function(){
-  showEvent();
-})
-})
-$(function(){
-$('#music_link').click(function(){
-  showMusic();
-})
-})
-$(function(){
-$('#menu_link').click(function(){
- showMenu();
-})
-})
-$(function(){
-$('#menu_link2').click(function(){
- showMenu();
-})
-})
-$(function(){
-$('#directions_return').click(function(){
- showMenu();
-})
-})
-$(function(){
-$('#bring_return').click(function(){
- showMenu();
-})
-})
-$(function(){
-$('#signup_return').click(function(){
- showMenu();
-})
-})
-$(function(){
-$('#rewards_return').click(function(){
- showMenu();
-})
-})
-$(function(){
-$('#opp_return').click(function(){
- showMenu();
-})
-})
-$(function(){
-$('#events_return').click(function(){
- showMenu();
-})
-})
-$(function(){
-$('#music_return').click(function(){
- showMenu();
-})
-})
-$(function(){
-$('#directions_link').click(function(){
- showDirections();
-})
-})
-$(function(){
-$('#bring_link').click(function(){
- showWhatToBring();
-})
-})
-$(function(){
-$('#volunteer_link').click(function(){
- showSignUp();
-})
-})
-$(function(){
- $('#landing_link').click(function(){
-   showMenu();
- })
-})
-$(function(){
- $('#opp_link').click(function(){
-   showOpportunities();
- })
-})
-$(function(){
- $('#rewards_link').click(function(){
-   showRewards();
- })
-})
+// $(function(){
+// $('#events_link').click(function(){
+//   showEvent();
+// })
+// })
+// $(function(){
+// $('#music_link').click(function(){
+//   showMusic();
+// })
+// })
+// $(function(){
+// $('#menu_link').click(function(){
+//  showMenu();
+// })
+// })
+// $(function(){
+// $('#menu_link2').click(function(){
+//  showMenu();
+// })
+// })
+// $(function(){
+// $('#directions_return').click(function(){
+//  showMenu();
+// })
+// })
+// $(function(){
+// $('#bring_return').click(function(){
+//  showMenu();
+// })
+// })
+// $(function(){
+// $('#signup_return').click(function(){
+//  showMenu();
+// })
+// })
+// $(function(){
+// $('#rewards_return').click(function(){
+//  showMenu();
+// })
+// })
+// $(function(){
+// $('#opp_return').click(function(){
+//  showMenu();
+// })
+// })
+// $(function(){
+// $('#events_return').click(function(){
+//  showMenu();
+// })
+// })
+// $(function(){
+// $('#music_return').click(function(){
+//  showMenu();
+// })
+// })
+// $(function(){
+// $('#directions_link').click(function(){
+//  showDirections();
+// })
+// })
+// $(function(){
+// $('#bring_link').click(function(){
+//  showWhatToBring();
+// })
+// })
+// $(function(){
+// $('#volunteer_link').click(function(){
+//  showSignUp();
+// })
+// })
+// $(function(){
+//  $('#landing_link').click(function(){
+//    showMenu();
+//  })
+// })
+// $(function(){
+//  $('#opp_link').click(function(){
+//    showOpportunities();
+//  })
+// })
+// $(function(){
+//  $('#rewards_link').click(function(){
+//    showRewards();
+//  })
+// })
 
 function hideAll(){
   $('#landing_page').fadeOut(750);
@@ -101,7 +101,7 @@ function hideAll(){
   $('#music').hide();
   $('#rewards').hide();
   $('#opportunities').hide();
-  $('#menuselection').html('');
+  $('#brentalfloss').hide();
 }
 
 function showMenu(){
@@ -117,50 +117,47 @@ function showMenu(){
 function showAbout(){
   hideAll();
   $('#about').show();
-  $('#menu').hide();
   $('#aboutselection').html('');
   cur_menu = 'about_menu';
 }
 function showEvent(){
   hideAll();
-  $('#about').hide();
   $('#events').show();
   cur_menu = 'return';
 }
 function showMusic(){
   hideAll();
-  $('#about').hide();
   $('#music').show();
-  cur_menu = 'return';
+  cur_menu = 'music_menu';
 }
 function showDirections(){
   hideAll();
   $('#directions').show();
-  $('#menu').hide();
   cur_menu = 'return';
 }
 function showWhatToBring(){
   hideAll();
   $('#whattobring').show();
-  $('#menu').hide();
   cur_menu = 'return';
 }
 function showSignUp(){
   hideAll();
   $('#signup').show();
-  $('#menu').hide();
   cur_menu = 'volunteer_menu';
 }
 function showOpportunities(){
   hideAll();
-  $('#signup').hide();
   $('#opportunities').show();
   cur_menu = 'return';
 }
 function showRewards(){
   hideAll();
-  $('#signup').hide();
   $('#rewards').show();
+  cur_menu = 'return';
+}
+function showBrentalfloss(){
+  hideAll();
+  $('#brentalfloss').show();
   cur_menu = 'return';
 }
 
@@ -187,6 +184,15 @@ $(window).hashchange(function(){
       break;
     case '#music':
       showMusic();
+      break;
+    case '#brentalfloss':
+      showBrentalfloss();
+      break;
+    case '#rewards':
+      showRewards();
+      break;
+    case '#opportunities':
+      showOpportunities();
       break;
   }
 })
@@ -254,29 +260,45 @@ window.onload = function(){
          $('#menuselection').html(e.key);
        }
        break;
+     case 'music_menu':
+       if (e.key == "Enter"){
+         switch($('#music_selection').html()){
+           case '1':
+             window.location.hash = "#brentalfloss"
+             break;
+         }
+       }else{
+         $('#music_selection').html(e.key);
+       }
+       break;
      case 'volunteer_menu':
        if (e.key == "Enter"){
          switch($('#volunteer_selection').html()){
            case '1':
-             showOpportunities();
-             cur_menu = 'return';
+             // showOpportunities();
+             // cur_menu = 'return';
+             window.location.hash = "#opportunities"
              break;
            case '2':
-             showRewards();
-             cur_menu = 'return';
+             // showRewards();
+             // cur_menu = 'return';
+             window.location.hash = "#rewards"
              break;
            case '3':
-             showMenu();
+             // showMenu();
+             window.location.hash = "#menu"
              break;
          }
        }else{
          $('#volunteer_selection').html(e.key);
        }
+       break;
      case 'return':
        if (e.key == ' '){
-         showMenu();
+         window.location.hash = "#menu";
        }
    }
  })
- setTimeout("showMenu()", 2000);
+ window.location.hash = '';
+ setTimeout("window.location.hash = '#menu'", 2000);
 }
